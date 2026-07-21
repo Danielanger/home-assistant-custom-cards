3d9e1f402abb84162148b071167a10a510ada919
Daniel Anger <daniel.anger@deutschebahn.com>
2026-07-21 10:40:56 +0200

    Revert "test"
    This reverts commit b76c92823d30b90c5b52afb11731ca2eaa1aff2b.

--- room-detail-card.js
+++ room-detail-card.js
@@ -1,4 +1,4 @@
-const ROOM_DETAIL_CARD_VERSION = "0.2.7";
+const ROOM_DETAIL_CARD_VERSION = "0.2.6";
 
 const clone = (value) => {
   if (typeof structuredClone === "function") {
@@ -437,10 +437,6 @@ class RoomDetailCard extends HTMLElement {
             primary_info: "none",
             secondary_info: "none",
             use_light_color: false,
-            show_brightness_control: false,
-            show_color_temp_control: false,
-            show_color_control: false,
-            collapsible_controls: false,
             tap_action: { action: "toggle" },
             hold_action: { action: "none" },
             double_tap_action: { action: "none" },
@@ -464,10 +460,8 @@ class RoomDetailCard extends HTMLElement {
             show_color_control: item.show_color_control !== false,
             show_brightness_control: item.show_brightness_control !== false,
             card_mod: {
-              style: {
-                "mushroom-card$ .actions": "padding-left: 10px;",
-                ".": "ha-card { margin-left: calc(-100% + 55px); transition-property: none !important; }",
-              },
+              style:
+                "ha-card { margin-left: calc(-100% + 55px); transition-property: none !important; }",
             },
           },
         ],
